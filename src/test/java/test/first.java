d8b71376824ae29e3903b1a7dde419916380abc3
VijayPriya <VijayPriya@10.0.0.187>
2020-08-18 15:14:48 -0400

    Added annotation

--- src/test/java/test/first.java
+++ src/test/java/test/first.java
@@ -7,11 +7,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class first {
 	
+	@Test
 	public void negativeLogin1() {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
